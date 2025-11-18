5ac1ad76cddc836dc0909272bdb7b7d4c54d4f8e
Xico Trujano <xtrujano@mac.com>
2025-11-18 11:43:41 -0600

    Fixing oscured email addresses

--- j/oscure-me.js
+++ j/oscure-me.js
@@ -1,3 +1,4 @@
+/*
 // Este script construye dinámicamente una dirección de correo electrónico
 document.addEventListener('DOMContentLoaded', () => {
   const link = document.getElementById("email-link");
@@ -15,3 +16,21 @@ document.addEventListener('DOMContentLoaded', () => {
 
   // Insertamos el texto visible
   document.getElementById("email-text").textContent = email;
+  */
+
+  // Este script construye dinámicamente una dirección de correo electrónico
+document.addEventListener('DOMContentLoaded', () => {
+  // Definimos las partes del correo
+  const user = "xico.trujano";
+  const domain = "proton.me";
+  const email = `${user}@${domain}`;
+
+  // Para todos los enlaces con la clase "email-link"
+  document.querySelectorAll(".email-link").forEach(link => {
+    link.setAttribute("rel", "noopener");
+    link.href = "mailto:" + email;
+
+    const span = link.querySelector(".email-text");
+    if (span) span.textContent = email;
+  });
+});
